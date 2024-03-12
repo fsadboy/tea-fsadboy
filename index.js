58a41213fcde376e0fff666595d184b195fc3c75
fsadboy <163062036+fsadboy@users.noreply.github.com>
2024-03-12 16:44:52 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const count = require('text-count');
+
+function fsadboy() {
+  const text = "F S A D B O Y"
+  return {
+    text,
+    length: count.charCount(text)
+  }
+}
+
+
+module.exports = Fsadboy
